e6c8bdda4bb2b6267990862dd2f949f2456432d6
Alfonso <alpegon@protonmail.com>
2020-06-20 17:02:26 +0200

    Add project files

---
+++ run_kubernetes.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Step 1:
+# This is your Docker ID/path
+dockerpath=alpegon/house-price-prediction
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run house-price-prediction\
+    --image=$dockerpath\
+    --port=80 --labels app=house-price-prediction
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward house-price-prediction 8000:80
